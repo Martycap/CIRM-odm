bb4e7f28d7a7b874f01c0ba3b8d7485f3a321c50
Martina Capone <mcapone@cybsec.it>
2025-08-04 15:10:48 +0000

    Aggiunti i modelli ODM

--- src/cirm_odm/__init__.py
+++ src/cirm_odm/__init__.py
@@ -3,6 +3,11 @@
 #   Licensed under the MIT License. See LICENSE in project root for information.
 #   -------------------------------------------------------------
 """Python Package Template"""
-from __future__ import annotations
+from .cpe_model import CPEModel, CPEMatch, CPEMatchingCondition
+from .cve_model import CVEModel
+from .cve_processing_results import CVEProcessingResults, CPEEntity, ProductWithPart, CVEPredictions
+from .cwe_model import CWEModel, Detection
+from .known_cpe import KnownCPE
+
 
 __version__ = "0.0.2"
